4ebdde678da8f22bb8b6d5d3e556e6b64385644c
dejoen <jaxjoe31@gmail.com>
2024-05-21 10:44:00 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,100 @@
-/*import { useState } from 'react'*/
+import { useState } from 'react'
 import NavBarComponent from './component/NavBarComponent.tsx'
 function App() {
  /* const [count, setCount] = useState(0)
 let name= "john"
   const [image,setImage] = useState(null)
   */
+  const [text,setText] = useState("nothing yet!!!")
+  const [users, updateUser] = useState([
+    {
+      id:1,
+      name:"joe",
+      isAdmin:false
+    },
+        {
+      id:2,
+      name:"jossy",
+      isAdmin:false
+        },
+    {
+      id:3,
+      name:"dejoe",
+      isAdmin:false
+    }
+    
+    ])
+    
+    let onButtonClicked = (e,id) =>{
+      updateUser(prevState=>{
+        return prevState.map(user=>{
+          if(user.id==id){
+           return  {
+              ...user,
+              isAdmin:!user.isAdmin
+            }
+          }
+           return user
+        })
+        /*prevState.map(user=>{
+         if(user.id==id){
+           /*return {
+              ...user,
+              isAdmin:true
+            }
+          }
+         return
+        })*/
+        
+      })
+        
+      
+    }
+  
    return (
 
     <>
   <NavBarComponent />
-  <div className="relative bg-blue-400 min-h-screen">
-  <p> body</p>
-  <div className="bg-amber-700  m-1.5 rounded sm:m-10 sm:bg-emerald-700 sm:h-96 h-fit" >
-
+  <div className="relative  min-h-screen">
+  <div className=" w-fit m-auto p-10 text-justify space-y-2 text-neutral-900" > 
+  <p> this is a new project that i will like to present. i am trying to build a nice app with nice flows.</p>
+   <p> this is a new project that i will like to present. i am trying to build a nice app with nice flows.</p>
+    <p> this is a new project that i will like to present. i am trying to build a nice app with nice flows.</p>
   </div>
+  <div className="bg-amber-700  m-1.5 rounded sm:m-10 sm:bg-emerald-700 sm:h-96 h-fit m-10 p-5" >
+     hello
+        <p> this is a new project that i will like to present. i am trying to build a nice app with nice flows.</p>
+    <p> this is a new project that i will like to present. i am trying to build a nice app with nice flows.</p>
+  </div>
+  
+    <div className="bg-amber-700  m-1.5 rounded sm:m-10 sm:bg-emerald-700 sm:h-96 h-fit m-10 p-5 w-auto" >
+      <input type="text-area" className="outline-0 h-auto p-3" onChange={
+         (e)=>{
+          setText(text=>{
+            return text=e.target.value
+          })
+         }
+        }/>
+      
+      <p>{text}</p>
+    </div>
+    
+    {
+      users.map(user=>(
+            <ul Key={user.id} className="bg-amber-700  m-1.5 rounded sm:m-10 sm:bg-emerald-700 sm:h-96 h-fit m-10 p-5 w-auto" onClick={(e)=>{
+              onButtonClicked(e,user.id)
+             // alert(e.target.Key)
+            }}>
+             <div >
+              <p>{user.name} </p>
+              <p>{user.isAdmin.toString()}</p>
+              </div>
+            
+    </ul>
+           
+        ))
+    }
+    
   </div>
    </>
   )
--- src/component/NavBarComponent.tsx
+++ src/component/NavBarComponent.tsx
@@ -6,7 +6,7 @@ let NavBarComponent = () =>{
     <Disclosure  as="nav" className="relative bg-gray-800 static top-0 z-1">
     {
       <div className="mx-auto max-w-7xl
-      px-2 sm:p-6 lg:px-8 bg-amber-400">
+      px-2 sm:p-6 lg:px-8 bg-amber-400 rounded-b-3xl">
       <div className= "relative flex h-16 items-center  justify-between" >
       <div className="absolute inset-y-0 left-0 flex items-center sm:hidden ">
       <Disclosure.Button className="absolute inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
